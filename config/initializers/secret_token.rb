70b58d65f36802e270d80fc234179133ae90d78e
Ben Redfield <bredfield@gmail.com>
2013-10-06 13:42:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Verbose::Application.config.secret_token = '3fdae1d551d6c4cc1945fe28a7343262f0f06f97647d1bd1b5388397427f2b2ea6f80a6fc96e06f3d92d55490f5b88a20bce47ae70ac8674e661ed821c9884d4'
